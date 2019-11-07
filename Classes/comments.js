2f9fc900ae74518630e922e447fed19517c19cdf
Scripter12 <elicardona57@gmail.com>
2019-11-06 21:54:53 -0500

    setup

---
+++ Classes/comments.js
@@ -0,0 +1,19 @@
+class Comments {
+  constructor() {
+    this.data = []
+  }
+  getAll() {
+    return this.data
+  }
+  add() {
+
+  }
+  edit() {
+
+  }
+  delete() {
+
+  }
+}
+
+module.exports = Comments;
