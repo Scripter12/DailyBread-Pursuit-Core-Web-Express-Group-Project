ca2af1b420bb6b918059e13e466724c098ea9b4f
Scripter12 <elicardona57@gmail.com>
2019-11-09 12:43:53 -0500

    removed classes folder

--- Routes/comments.js
+++ Routes/comments.js
@@ -1,5 +1,20 @@
 const express = require('express')
+const pgp = require('pg-promise')
+const postClass = require("../Classes/comments.js");
 const router = express.Router()
+const connectionString = "postgres://localhost:5432/dailyBread"
+const db = pgp(connectionString);
 
+
+
+router.get("/posts/:post_id", (req, res) => {
+
+})
+
+router.post("/posts/:post_id/:commenter_id")
+
+router.patch(":post_id/commenter_id")
+
+router.delete(":post_id/commenter_id")
 //Export
 module.exports = router
