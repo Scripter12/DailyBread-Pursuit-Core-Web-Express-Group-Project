d852580e5970920b4a78f9086d4a2532e9afb8a8
kadjiah <kadijahwilson@pursuit.org>
2019-11-10 15:24:13 -0500

    saving changes

--- Routes/users.js
+++ Routes/users.js
@@ -1,9 +1,6 @@
 const express = require('express')
 const router = express.Router()
-
-const pgp = require('pg-promise')()
-const connectionString ="postgres://localhost:5432/dailybread"
-const db = pgp(connectionString);
+const db = require('./db')
 
 router.get('', async (req, res) => {
     try{
