0d4bc10bd3ad8624eea1f31f5dbdcf8a2090192d
Scripter12 <elicardona57@gmail.com>
2019-11-10 11:48:20 -0500

    work on comments

--- Routes/pictures.js
+++ Routes/pictures.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const pgp = require('pg-promise')();
+const connectionString = "postgres://localhost:5432/dailybread"
+const db = pgp(connectionString);
+
 
 //Export
 module.exports = router
