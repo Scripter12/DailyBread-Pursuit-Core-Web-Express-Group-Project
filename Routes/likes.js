a902be79f8a5f73f1736e0be5e1d4f7ddb7452ba
maliq <maliqtaylor@pursuit.org>
2019-11-11 20:43:19 -0500

    update

--- Routes/likes.js
+++ Routes/likes.js
@@ -3,20 +3,40 @@ const router = express.Router()
 const db = require('./db')
 
 
-router.get('/likes/posts/:post_id', (req,res) =>{
+router.get('/posts/:post_id', async (req,res) =>{
     let post_id = req.params.post_id
     try{
-        let likes = db.any(`SELECT * FROM likes WHERE post_id = ${post_id}`)
+        let likes = await db.any(`SELECT * FROM likes WHERE post_id = ${post_id}`)
         res.json({
-
             data : likes,
         })
-    }catch{
-        (error){
+    }catch(error){
+        res.json({
+            'error' : error,
+        })
+    }
+})
+
+router.post('/posts/:post_id', async (req,res) =>{
+    let post_id = req.params.post_id
+    try{
+        let insertQuery = `INSERT into likes(liker_id, post_id)
+                VALUES($1, $2)`
+        if (!insertQuery) {
             res.json({
-                'error' : error,
+                message: "information Missing"
+            })
+
+        }else {
+            await db.none(insertQuery, [req.body.liker, req.params.post_id])
+            res.json({
+                message: "liked"
             })
         }
+    } catch (error) {
+        res.json({
+            message: error
+        })
     }
 })
 
--- front-end/home.js
+++ front-end/home.js
@@ -2,6 +2,17 @@ const onClickMenu= () => {
     document.querySelector('#menu').classList.toggle('change');
     
     document.querySelector('#nav').classList.toggle('change');
+
+    document.querySelector('#menu-text').classList.toggle('change');
+    
+    let menu_text = document.querySelector('#menu-text')
+    
+    if(menu_text.classList.value){
+        menu_text.innerText = ''
+    }else{
+        menu_text.innerText = 'Menu'
+    }
+    
 }
 
 
