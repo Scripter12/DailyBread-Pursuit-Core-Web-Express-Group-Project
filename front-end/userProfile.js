7cd491edbb8a9e27abff5ff7274936c1846ada65
kadjiah <kadijahwilson@pursuit.org>
2019-11-12 19:12:37 -0500

    frontend update

--- front-end/userProfile.js
+++ front-end/userProfile.js
@@ -1,1 +1,17 @@
-function 
+const onClickMenu= () => {
+    document.querySelector('#menu').classList.toggle('change');
+    
+    document.querySelector('#nav').classList.toggle('change');
+
+    document.querySelector('#menu-text').classList.toggle('change');
+    
+    let menu_text = document.querySelector('#menu-text')
+    
+    if(menu_text.classList.value){
+        menu_text.innerText = ''
+    }else{
+        menu_text.innerText = 'Menu'
+    }
+    
+}
+
